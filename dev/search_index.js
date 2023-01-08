85dc7b61cd956a1b407f6aab38000979b22fbbbb
Documenter.jl <documenter@juliadocs.github.io>
2023-01-08 21:38:40 +0000

    build based on 7c6ae57

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = VisualizeMotifs","category":"page"},{"location":"#VisualizeMotifs","page":"Home","title":"VisualizeMotifs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for VisualizeMotifs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [VisualizeMotifs]","category":"page"}]
+}
